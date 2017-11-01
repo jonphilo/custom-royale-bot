69ea4a24cf44f89ecf2c4b9d6b80c75c47e6a770
Jonathan Philo <Jonathan.Philo@gmail.com>
2017-10-31 22:42:20 -0400

    implemented basic game functionality.

--- bot.js
+++ bot.js
@@ -11,22 +11,99 @@ client.on('message', message => {
     console.log(message.content);
     if(message.content.substring(0,1) == '.')
     {
-        var args = message.content.substring(1).split('~');
+        var args = message.content.substring(1).split('-');
+        TrimArgumentSpaces(args);
         var cmd = args[0];
 
         args = args.splice(1);
 
         switch(cmd){
+            case 'getall':
+                GetAllCommand(message, args);
+                break
             case 'randomgame':
-                message.reply(RandomGameCommand(message, args));
+                RandomGameCommand(message, args);
+                break;
             case 'registergame':
                 RegisterGameCommand(message, args);
                 message.reply('Game Registered');
+                break;
             default:
                 break;
         }
     }
 });
+
+function deleteMessages(message)
+{
+    message.channel.fetchMessages()
+        .then(messages => message.channel.bulkDelete(messages));
+}
+
+function TrimArgumentSpaces(args)
+{
+    for(var i = 0; i < args.length; i++)
+    {
+        args[i] = args[i].trim();
+    }
+}
+
+function GetAllCommand(message, args)
+{
+    // Set the headers
+    var headers = {
+        'User-Agent':       'Super Agent/0.0.1',
+        'Content-Type':     'application/x-www-form-urlencoded'
+    }
+    
+    // Configure the request
+    var options = {
+        url: 'http://localhost:8080/api/gamemodes',
+        method: 'GET',
+        headers: headers,
+        form: {}
+    }
+    var discordResponse = '';
+    // Start the request
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var jsonData =  JSON.parse(body);
+            for (var i = 0; i < jsonData.length; i++) {
+                var gameMode = jsonData[i];
+                discordResponse = discordResponse + '**' + gameMode.Name + ' - **' + gameMode.Description + ' by ' + gameMode.Submitter + '\n';
+            }
+            deleteMessages(message);
+            message.channel.send(discordResponse);
+            
+        }
+    });
+}
+
+function RandomGameCommand(message, args)
+{
+    // Set the headers
+    var headers = {
+        'User-Agent':       'Super Agent/0.0.1',
+        'Content-Type':     'application/x-www-form-urlencoded'
+    }
+    
+    // Configure the request
+    var options = {
+        url: 'http://localhost:8080/api/gamemodes/random',
+        method: 'GET',
+        headers: headers,
+        form: {}
+    }
+    var discordResponse = '';
+    // Start the request
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var gameMode =  JSON.parse(body);
+            discordResponse = discordResponse + '**' + gameMode.Name + ' - **' + gameMode.Description + ' by ' + gameMode.Submitter + '\n';
+            message.reply(discordResponse);            
+        }
+    });
+}
 
 function RegisterGameCommand(message, args)
 {    
@@ -50,10 +127,10 @@ function RegisterGameCommand(message, args)
     // Start the request
     request(options, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            // Print out the response body
+            // Print out the response body 
             console.log(body)
         }
     })
 }
 
-client.login('[token here]');
+client.login('[Token Here]');
