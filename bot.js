7f1903b865c31861cbb891f06dcfc61ef91cf252
Jonathan Philo <jonathan.philo@outlook.com>
2017-11-10 14:43:30 -0500

    updated bot.js with admin commands and 2000 char limit

--- bot.js
+++ bot.js
@@ -22,11 +22,16 @@ client.on('message', message => {
                 GetAllCommand(message, args);
                 break
             case 'randomgame':
+            case 'rando':
                 RandomGameCommand(message, args);
                 break;
             case 'registergame':
                 RegisterGameCommand(message, args);
                 message.reply('Game Registered');
+                break;
+            case 'clr':
+            case 'clear':
+                ClearMessagesCommand(message);
                 break;
             default:
                 break;
@@ -48,8 +53,17 @@ function TrimArgumentSpaces(args)
     }
 }
 
+function ClearMessagesCommand(message)
+{
+    deleteMessages(message);
+}
+
 function GetAllCommand(message, args)
 {
+    if(!IsAdminRole(message))
+    {
+        return;
+    }
     // Set the headers
     var headers = {
         'User-Agent':       'Super Agent/0.0.1',
@@ -64,17 +78,21 @@ function GetAllCommand(message, args)
         form: {}
     }
     var discordResponse = '';
+    var maxCharacterLimit = 1500; //discord sets the max post limit to 2000, 1500 gives buffer room.
     // Start the request
     request(options, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var jsonData =  JSON.parse(body);
+            deleteMessages(message);
             for (var i = 0; i < jsonData.length; i++) {
                 var gameMode = jsonData[i];
                 discordResponse = discordResponse + '**' + gameMode.Name + ' - **' + gameMode.Description + ' by ' + gameMode.Submitter + '\n';
+                if(discordResponse.length > maxCharacterLimit){
+                    message.channel.send(discordResponse);
+                    discordResponse = '';
+                }
             }
-            deleteMessages(message);
             message.channel.send(discordResponse);
-            
         }
     });
 }
@@ -131,6 +149,17 @@ function RegisterGameCommand(message, args)
             console.log(body)
         }
     })
+}
+
+function IsAdminRole(message)
+{
+    var adminRoleName = 'Admin';
+    var adminRole = message.guild.roles.find(role => role.name == adminRoleName);
+    if(adminRole == null)
+    {
+        return false;
+    }
+    return true;
 }
 
 client.login('[Token Here]');
